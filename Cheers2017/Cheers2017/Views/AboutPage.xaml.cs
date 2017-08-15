aaac3976efc69325fccbca85be3577610c7e3399
Steven Rutherford <steven.rutherford@slalom.com>
2017-08-15 12:10:38 -0400

    vs studio generated

---
+++ Cheers2017/Cheers2017/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Cheers2017
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
